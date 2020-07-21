8070087c398536be4961101406c545ae37c820de
k1574 <k1574@yandex.ru>
2020-07-21 09:28:40 +0500

    Init.

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+enum{
+	MAXSTRLEN = 1024
+} ;
+
+char *argv0;
+
+void
+usage()
+{
+	fprintf(stderr, "Usage: %s [ASCII string]\n", argv0);
+	exit(1);
+}
+
+void
+main(int argc, char *argv[])
+{
+	int i, l, n, buflen;
+	char *s;
+	char buf[BUFSIZ];
+	argv0 = argv[0] ;
+	if(argc==1){
+		buflen = sizeof(buf)/sizeof(buf[1]) ;
+		do{
+			n = fread(buf, sizeof(buf[0]), buflen, stdin) ;
+			for( i=0 ; i<n ; ++i )
+				printf("%d\n", buf[i]);
+		}while( n == buflen );
+	} else if( argc==2) {
+		s = argv[1] ;
+		l = strnlen(s, MAXSTRLEN) ;
+		for(i=0 ; i<l ; ++i)
+			printf("%d\n", s[i]);
+	} else
+		usage();
+	exit(1);
+}
